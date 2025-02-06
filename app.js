4f822c1ba29e2d9b5ecc00c35920464a8cbe784f
Dmitry Alexeev <strokestronger@gmail.com>
2025-02-06 23:15:15 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+// Настройка статических файлов
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Маршрут для главной страницы
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Запуск сервера
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
+});
